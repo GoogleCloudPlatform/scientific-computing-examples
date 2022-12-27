0e42204d1a9b17ba63c0e4e2f52c9a095f1663bf
wkharold <wkharold@gmail.com>
2022-12-27 16:36:46 +0000

    Add a JupyterLab example (#34)
    * Functional changes for user specified compute node properties
    
    * Add support for user-defined properties and an MPI example
    
    * foundation, initial checkin
    
    * Get rid of duplicated per-example foundation components
    
    * Update READMEs to refer to example foundation
    
    * Add the foundation-based examples
    
    * Add top-level foundation init target
    
    * Top-level doc and foundation README
    
    * Fix heading error
    
    * Build a separate login image
    
    * Add hostbased authentication configuration scripts
    
    * Convert login_node_specs to an object
    
    * Use the new login_node_spec structure in examples
    
    * Add JupyterLab Example
    
    * Add a few more words
    
    * Add links, fix words
    
    * Add words about the Conda licence
    
    Brief explanation of the Conda licence review/acceptance interaction.
    
    Co-authored-by: Ward K Harold <wkh@google.com>

---
+++ fluxfw-gcp/tf/examples/notebooks/main.tf
@@ -0,0 +1,39 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module "cluster" {
+    source = "../.."
+
+    project_id           = data.terraform_remote_state.network.outputs.subnet.project
+    region               = data.terraform_remote_state.network.outputs.subnet.region
+    
+    service_account_emails = {
+        manager = data.terraform_remote_state.iam.outputs.management_service_account
+        login   = data.terraform_remote_state.iam.outputs.login_service_account
+        compute = data.terraform_remote_state.iam.outputs.compute_service_account
+    }
+    
+    subnetwork           = data.terraform_remote_state.network.outputs.subnet.self_link
+    cluster_storage      = data.terraform_remote_state.storage.outputs.cluster_storage
+
+    manager_name_prefix  = var.manager_name_prefix
+    manager_machine_type = var.manager_machine_type
+    manager_scopes       = var.manager_scopes
+
+    login_node_specs     = var.login_node_specs
+    login_scopes         = var.login_scopes
+
+    compute_node_specs   = var.compute_node_specs
+    compute_scopes       = var.compute_scopes
+}
