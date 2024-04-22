21a09c92ce835aa7f6d8b48bae3cd24c1704eb30
J Ross Thomson <39315853+jrossthomson@users.noreply.github.com>
2024-04-22 18:04:25 -0400

    Update fine-tune-slurm.sh

--- llama2-finetuning-slurm/files/fine-tune-slurm.sh
+++ llama2-finetuning-slurm/files/fine-tune-slurm.sh
@@ -2,10 +2,11 @@
 
 #SBATCH --job-name=llama2-fine-tune
 #SBATCH --nodes=1
-#SBATCH --partition=g2
+#SBATCH --partition=g2gpu8
 #SBATCH --time=1:10:00
 
-source ~/.bashrc
+export CONDA_BASE=/opt/conda
+source $CONDA_BASE/bin/activate base
 conda activate llama2
 cd $SLURM_SUBMIT_DIR
 python /data_bucket/fine-tune.py
