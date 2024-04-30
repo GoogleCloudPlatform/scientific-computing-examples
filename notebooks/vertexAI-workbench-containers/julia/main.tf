f5f648b20658e2815c9bc38d27fcd20e5f0268ae
jrossthomson <jrossthomson@google.com>
2024-04-30 11:17:01 -0400

    Ready for Mark to test

--- notebooks/vertexAI-workbench-containers/julia/main.tf
+++ notebooks/vertexAI-workbench-containers/julia/main.tf
@@ -8,15 +8,22 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "3.0.2"
     }
+    random = {
+      source = "hashicorp/random"
+    }
   }
 }
 
 locals {
   #us-central1-docker.pkg.dev/openfoam-jrt/my-repository
   repo_url = "${google_artifact_registry_repository.my-repo.location}-docker.pkg.dev"
-  repo_path = "${local.repo_url}/${google_artifact_registry_repository.my-repo.name}"
+  repo_path = "${local.repo_url}/${data.google_project.project.name}/${google_artifact_registry_repository.my-repo.name}"
+  suffix         = random_id.resource_name_suffix.hex 
 }
 
+resource "random_id" "resource_name_suffix" {
+  byte_length = 4
+}
 
 resource "google_artifact_registry_repository" "my-repo" {
   location      = "us-central1"
@@ -24,40 +31,42 @@ resource "google_artifact_registry_repository" "my-repo" {
   description   = "example docker repository"
   format        = "DOCKER"
   docker_config {
-    immutable_tags = true
+    immutable_tags = false
   }
 }
 
 resource "docker_image" "vertex_julia" {
   provider = docker
-  name     = "${local.repo_path}/vertex-julia:1.0"
+  name     = "${local.repo_path}/vertex-julia:${local.suffix}"
   build {
     context   = path.cwd
   }
 }
 
-#resource "docker_registry_image" "vertex_julia" {
-#  name                 = docker_image.vertex_julia.name
-#  insecure_skip_verify = true
-#  keep_remotely        = true
-#}
+ resource "null_resource" "build_and_push_image" {
+   provisioner "local-exec" {
+     command = <<-EOT
+     cd ${path.cwd}
+     gcloud builds submit --tag ${docker_image.vertex_julia.name} .
+     gcloud auth configure-docker us-central1-docker.pkg.dev
+     docker push ${docker_image.vertex_julia.name}
+     EOT
+   }
+ }
 
-resource "null_resource" "build_and_push_image" {
-  provisioner "local-exec" {
-    command = <<-EOT
-    cd ${path.cwd}
-    gcloud builds submit --tag ${docker_image.vertex_julia.name} .
-    gcloud auth configure-docker us-central1-docker.pkg.dev
-    docker push ${docker_image.vertex_julia.name}
-    EOT
-    
+resource "google_workbench_instance" "instance" {
+  name = "workbench-instance"
+  location = "us-west1-a"
+
+  gce_setup {
+    container_image {
+      repository = docker_image.vertex_julia.name
+    }
   }
-  
 }
 
 output "repo_name" {
-  #us-central1-docker.pkg.dev/openfoam-jrt/my-repository
-  value = local.repo_url
+  value = local.repo_path
 }
 
 output "repo_url" {
