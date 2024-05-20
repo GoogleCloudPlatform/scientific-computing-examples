b25d9772dd958622dd43afed5e3e880008eb55d9
jrossthomson <jrossthomson@google.com>
2024-05-20 14:54:17 -0400

    changed  version

--- notebooks/vertexAI-workbench-containers/julia/workbench/versions.tf
+++ notebooks/vertexAI-workbench-containers/julia/workbench/versions.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "= 4.84.0"
+      version = ">= 4.84.0"
     }
     random = {
       source  = "hashicorp/random"
