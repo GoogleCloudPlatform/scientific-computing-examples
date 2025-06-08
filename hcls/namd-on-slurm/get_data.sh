1f880a6ce2611771f66305e8329fe1cab171d626
jrossthomson <jrossthomson@google.com>
2025-06-08 14:56:37 +0000

    Updated build to new slurm image

--- hcls/namd-on-slurm/get_data.sh
+++ hcls/namd-on-slurm/get_data.sh
@@ -4,3 +4,5 @@
 
 wget -qO- https://www.ks.uiuc.edu/Research/namd/benchmarks/systems/apoa1_gpu.tar.gz | tar xvz
 wget -qO- https://www.ks.uiuc.edu/Research/namd/benchmarks/systems/stmv_gpu.tar.gz | tar xvz
+
+cp apoa1_gpures_npt.namd apoa1_gpu
