98ce9bcf80400dbbc4a043023f6a1cd901e7de57
jrossthomson <jrossthomson@google.com>
2025-08-01 19:40:29 +0000

    test

---
+++ hcls/parabricks-on-batch/setup.sh
@@ -0,0 +1,7 @@
+export GCSPATH=`uuid | tr -d '-' | head -c 8`
+
+gsutil mb gs://${GCSPATH}
+wget -o ${GCSPATH}.fasta https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/${GCSPATH}.fasta 
+gsutil cp ${GCSPATH}.fasta gs://${GCSPATH}/Homo_sapiens_assembly38.fasta
+
+envsubst < smi_temp.json > smi.json
