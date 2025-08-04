bd2998307c91c14073842fd10bbc45bd2bbbcb6b
jrossthomson <jrossthomson@google.com>
2025-08-04 15:34:25 +0000

    test

--- hcls/parabricks-on-batch/setup.sh
+++ hcls/parabricks-on-batch/setup.sh
@@ -1,13 +1,15 @@
 export GCSPATH=`uuid | tr -d '-' | head -c 8`
 
 gsutil mb gs://${GCSPATH}
-#curl https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta | gsutil cp - gs://${GCSPATH}/Homo_sapiens_assembly38.fasta &
-#curl https://42basepairs.com/download/gs/deepvariant/benchmarking/fastq/wgs_pcr_free/30x/HG001.novaseq.pcr-free.30x.R1.fastq.gz | gsutil cp - gs://${GCSPATH}/HG001.novaseq.pcr-free.30x.R1.fastq.gz&
-#curl https://42basepairs.com/download/gs/deepvariant/benchmarking/fastq/wgs_pcr_free/30x/HG001.novaseq.pcr-free.30x.R2.fastq.gz | gsutil cp - gs://${GCSPATH}/HG001.novaseq.pcr-free.30x.R2.fastq.gz&
+
+# From Broad public datasets: https://console.cloud.google.com/marketplace/product/broad-institute/references
+
+# Use Bucket to bucket transfer, extremely fast. 
+
+gsutil cp gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta gs://${GCSPATH}/
 
 # From https://42basepairs.com/search?query=.fastq&bucket=gs/deepvariant
 
-gsutil cp gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta gs://${GCSPATH}/
 gsutil cp gs://deepvariant/benchmarking/fastq/wgs_pcr_free/30x/HG001.novaseq.pcr-free.30x.R1.fastq.gz gs://${GCSPATH}/
 gsutil cp gs://deepvariant/benchmarking/fastq/wgs_pcr_free/30x/HG001.novaseq.pcr-free.30x.R2.fastq.gz gs://${GCSPATH}/
 
